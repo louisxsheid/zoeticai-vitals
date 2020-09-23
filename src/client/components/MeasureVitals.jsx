57676318bdbb1ed597df7bd939d8ac153afe197e
Louis Xavier Sheid III <louisxsheid@gmail.com>
2020-09-23 14:15:41 -0700

    initial card set-up

--- src/client/components/MeasureVitals.jsx
+++ src/client/components/MeasureVitals.jsx
@@ -1,15 +1,63 @@
-import React from 'react';
-import { Text, StyleSheet, Button } from 'react-native';
-
+import React from "react";
+import { StyleSheet, View, Text, SafeAreaView, Button } from "react-native";
+import { FlatGrid } from "react-native-super-grid";
 import { Center } from './Center';
 
+
 export const MeasureVitals = ({navigation}) => {
-  return(
-    <Center>
-      <Text style={{color: 'black', fontSize: 25}}>
-        Measure your vitals
-      </Text>
+  const [items, setItems] = React.useState([
+    { name: "Temperature", code: "#c0392b", data: "97.6" },
+    { name: "Blood Pressure", code: "#e74c3c", data: "124/40" },
+    { name: "Oximeter", code: "#3498db", data: "96% 78bpm" },
+  ]);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text style={styles.title}>Measure your vitals</Text>
+      <FlatGrid
+        itemDimension={200}
+        data={items}
+        style={styles.gridView}
+        // staticDimension={300}
+        // fixed
+        spacing={10}
+        renderItem={({ item }) => (
+          <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
+            <Text style={styles.itemName}>{item.name}</Text>
+            <Text style={styles.itemCode}>{item.data}</Text>
+          </View>
+        )}
+      />
       <Button title='go back' onPress={() => navigation.goBack()}/>
-    </Center>
-  )
-}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  gridView: {
+    marginTop: 10,
+    // backgroundColor: 'white',
+  },
+  itemContainer: {
+    // backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 10,
+    height: 225,
+  },
+  itemName: {
+    fontSize: 20,
+    color: "#fff",
+    fontWeight: "600",
+  },
+  itemCode: {
+    fontWeight: "600",
+    fontSize: 50,
+    color: "#fff",
+  },
+  title: {
+    fontSize: 25, 
+    fontWeight: 'bold',
+    textAlign: 'center',
+    width: '100%',
+  }
+});
