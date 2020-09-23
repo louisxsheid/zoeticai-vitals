ba8cbd110604d239db9ee8960370be1acd1ba951
Louis Xavier Sheid III <louisxsheid@gmail.com>
2020-09-23 08:56:22 -0700

    add TodayDate component

---
+++ src/client/components/TodayDate.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { 
+  Dimensions,
+  StyleSheet, 
+  SafeAreaView, 
+  Platform,
+  StatusBar,
+  View,
+  Text
+} from 'react-native';
+
+import { 
+  useDimensions, 
+  useDeviceOrientation 
+} from '@react-native-community/hooks';
+
+const moment = require('moment');
+
+export const TodayDate = () => {
+
+  const { landscape } = useDeviceOrientation();
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* <Text style={{ fontSize: 15, color: 'black'}}>
+        Today is
+      </Text> */}
+      <Text style={styles.text}>
+      {moment().format('LL')}
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: 'dodgerblue',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight: 0,
+    alignItems: 'center',
+    width: '100%',
+    height: 40,
+    // justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black'
+  }
+});
--- src/client/components/Vitals.jsx
+++ src/client/components/Vitals.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, SafeAreaView, View } from 'react-native';
 
 import { Center } from '../components/Center';
+import { TodayDate } from '../components/TodayDate'
 
 export const Vitals = () => {
   return(
-    <Center>
-      <Text>Vitals page</Text>
-    </Center>
+      <TodayDate/>
   )
 }
